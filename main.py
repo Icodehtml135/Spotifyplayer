7c00e019e64a6ab5285afb24d4ffea987b90168f
Icodehtml135 <sapkalishan@gmail.com>
2025-08-04 12:24:19 +1200

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ def index():
     return render_template("index.html", playlist_id=playlist_id)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+import os 
+
+port  = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
